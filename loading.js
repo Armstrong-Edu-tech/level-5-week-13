4bdc2a29d9ef1578f1bf1c9fb3bb3a56f5942ded
Yara Megawer <108768001+yaramegawer@users.noreply.github.com>
2026-02-17 14:57:34 +0200

    Create loading.js

---
+++ loading.js
@@ -0,0 +1,8 @@
+// app/dashboard/loading.js
+export default function Loading() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <p>Loading content, please wait...</p>
+    </div>
+  )
+}

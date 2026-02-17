72ed77eed2f0e4e40f27a9ba2b6b168bfd2bf655
Yara Megawer <108768001+yaramegawer@users.noreply.github.com>
2026-02-17 20:55:08 +0200

    Add files via upload

---
+++ loading.js
@@ -0,0 +1,9 @@
+// app/dashboard/loading.js
+
+export default function Loading(){
+    return(
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <p>Loading content, please wait...</p>
+    </div>
+    )
+}

cf1141de001b7188b0527353604ea2393b3606b4
Yara Megawer <108768001+yaramegawer@users.noreply.github.com>
2026-02-20 12:18:41 +0200

    Create weekly project login.js

---
+++ weekly project login.js
@@ -0,0 +1,10 @@
+import LoginForm from "./LoginForm";
+
+export const metadata = {
+  title: "Login | NextShop",
+  description: "Login to access your account",
+};
+
+export default function LoginPage() {
+  return <LoginForm />;
+}

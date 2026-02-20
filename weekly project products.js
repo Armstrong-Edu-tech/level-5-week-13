72b415a4d2328d92620d40a5dcfcdea3e7a7ea3e
Yara Megawer <108768001+yaramegawer@users.noreply.github.com>
2026-02-20 12:19:10 +0200

    Create weekly project products.js

---
+++ weekly project products.js
@@ -0,0 +1,10 @@
+import Products from "./Products";
+
+export const metadata = {
+  title: "Products | NextShop",
+  description: "Your Products",
+};
+
+export default function LoginPage() {
+  return <Products />;
+}
